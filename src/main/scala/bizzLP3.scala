eae428bd36a8bff0dd7f8a252babeb59bef3f7d2
Miguel Iglesias <miguel.iglesias@dimeder.com>
2019-03-06 18:07:51 -0500

    looking nice!

--- src/main/scala/bizzLP3.scala
+++ src/main/scala/bizzLP3.scala
@@ -3,38 +3,32 @@ package bizz7
 import scalaz.syntax.std.boolean.ToBooleanOpsFromBoolean
 import ammonite.ops._
 
+sealed trait Res[A]
+case class Fail[A]() extends Res[A]
+case class Suc[A]( a: A ) extends Res[A]
+
 object resolver {
-  trait Lookup[C, S, A, F[_]] {
-    def lookup( c: C, f: C => F[S] ): F[A]
-    def success[R]( f: A => F[R] ): F[A] => F[R]
-    def failure[R]( f: () => R ): () => R
-    def repr( s: S ): A
+  trait Resolver[F[_], G[_]] {
+    def create[C, S]( f: C => G[S] ): F[C => G[S]]
+    def transition[C, S, Z]( f: F[C => G[S]], g: S => Z ): F[C => G[S => Z]]
+    def choose[C, S, S1, S2, Z](
+      origin:   F[C => G[S => Z]],
+      fallback: F[C => G[S1 => Z]],
+      next:     F[C => G[( S => G[S2] ) => ( S2 => Z )]]
+    ): F[C => G[Z]]
   }
   trait Process[A, R] {
     def process: A => R
   }
 }
-
+/*
 object states {
   import models._
   import resolver._
-  implicit object lookupState1 extends Lookup[Context, State1, State1, Option] {
-    def lookup( c: Context, f: Context => Option[State1] ) = f( c ).map( repr )
-    def repr( s: State1 ) = s
-    def success[R]( f: State1 => Option[R] ) = fa => fa.flatMap( f )
-    def failure[R]( f: () => R ) = () => f()
-  }
-  implicit object lookupState2 extends Lookup[Context, State2, State2, Option] {
-    def lookup( c: Context, f: Context => Option[State2] ) = f( c ).map( repr )
-    def repr( s: State2 ) = s
-    def success[R]( f: State2 => Option[R] ) = fa => fa.flatMap( f )
-    def failure[R]( f: () => R ) = () => f()
-  }
-  implicit object lookupState3 extends Lookup[State1, State3, State3, Option] {
-    def lookup( c: State1, f: State1 => Option[State3] ) = f( c ).map( repr )
-    def repr( s: State3 ) = s
-    def success[R]( f: State3 => Option[R] ) = fa => fa.flatMap( f )
-    def failure[R]( f: () => R ) = () => f()
+  case class R[A]( unR: A )
+  implicit object lookupState extends Resolver[R, Option] {
+    def lookup[C, S]( f: C => Option[S] ) = R( f )
+    def apply[C, S]( c: C )( f: R[C => Option[S]] ): Option[S] = f.unR( c )
   }
   implicit object process1 extends Process[State1, Response] {
     def process = s => Response( "state1" )
@@ -50,57 +44,49 @@ object states {
 object strings {
   import models._
   import resolver._
-  implicit object lookupState1 extends Lookup[Context, State1, String, Option] {
-    def lookup( c: Context, f: Context => Option[State1] ) = f( c ).map( repr )
-    def repr( s: State1 ) = s.toString
-    def success[R]( f: String => Option[R] ) = fa => fa.flatMap( f )
-    def failure[R]( f: () => R ) = () => f()
-
-  }
-  implicit object lookupState2 extends Lookup[Context, State2, String, Option] {
-    def lookup( c: Context, f: Context => Option[State2] ) = f( c ).map( repr )
-    def repr( s: State2 ) = s.toString
-    def success[R]( f: String => Option[R] ) = fa => fa.flatMap( f )
-    def failure[R]( f: () => R ) = () => f()
-  }
-  implicit object lookupState3 extends Lookup[String, State3, String, Option] {
-    def lookup( c: String, f: String => Option[State3] ) = f( c ).map( s => s"$c => ${repr( s )}" )
-    def repr( s: State3 ) = s.toString
-    def success[R]( f: String => Option[R] ) = fa => fa.flatMap( f )
-    def failure[R]( f: () => R ) = () => f()
-  }
+  case class Str[A]( unS: A )
+// implicit object lookupState extends Resolver[Str, Str] {
+//   def lookup[C, S]( f: C => Option[S] ) = Str( f )
+//   def apply[C, S]( c: C )( f: Str[C => Option[S]] ): Option[S] = f.unS( c )
+// }
   implicit object process extends Process[String, String] {
     def process = s => s"Response for $s"
   }
 }
-
+*/
 object program {
   import models._
   import resolver._
-  def graph[S1, S2, S3, R, F[_]](
-    c:  Context,
-    f1: Context => F[State1],
-    f2: Context => F[State2],
-    f3: S1 => F[State3]
+  import scalaz._, Scalaz._
+  def graph[F[_], R](
+    f1: Context => Option[State1],
+    f2: Context => Option[State2],
+    f3: Context => Option[State1 => Option[State3]]
   )(
     implicit
-    L1: Lookup[Context, State1, S1, F],
-    L2: Lookup[Context, State2, S2, F],
-    L3: Lookup[S1, State3, S3, F],
-    P1: Process[S1, R],
-    P2: Process[S2, R],
-    P3: Process[S3, R]
-  ): F[R] = {
-    val l1: F[S1] = L1.lookup( c, f1 )
-    val l2: () => F[S2] = L1.failure( () => L2.lookup( c, f2 ) )
-    val l3: F[S1] => F[S3] = L1.success( s1 => L3.lookup( s1, f3 ) )
-    ???
+    L:  Resolver[F, Option],
+    P1: Process[State1, R],
+    P2: Process[State2, R],
+    P3: Process[State1 => Option[State3], State3 => R]
+
+  ): F[Context => Option[R]] = {
+    val s1: F[Context => Option[State1]] = L.create( f1 )
+    val s2: F[Context => Option[State2]] = L.create( f2 )
+    val s3: F[Context => Option[State1 => Option[State3]]] = L.create( f3 )
+    val l1: F[Context => Option[State1 => R]] = L.transition( s1, P1.process )
+    val l2: F[Context => Option[State2 => R]] = L.transition( s2, P2.process )
+    val l3: F[Context => Option[( State1 => Option[State3] ) => ( State3 => R )]] = L.transition( s3, P3.process )
+    L.choose( l1, l2, l3 )
   }
 }
 
 object app extends App {
-  import models.Context
+  import models._
   import models.values._
-  import strings._
-  //println( program.graph( Context( users, Nil ) ) )
+  //import strings._
+  // println( program.graph[Str, String]( Context( users, Nil ) )(
+  //   f1 = _.dialog.isEmpty.option( State1() ),
+  //   f2 = _.dialog.nonEmpty.option( State2() ),
+  //   f3 = _ => None
+  // ) )
 }
